8552d42eb42a5575e5084bd30e778a6294f222ab
ericgaspar <junk.eg@free.fr>
2020-11-28 10:19:04 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,11 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="postgresql libgd2-xpm-dev"
+pkg_dependencies="postgresql postgresql apt-transport-https libgd-dev"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php-gettext"
+extra_php_dependencies="php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-gd"
 
 #=================================================
 # COMMON HELPERS
