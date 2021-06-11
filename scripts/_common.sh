9765f7a63f59c6d20e3b3692736193cd07ef03e8
Alexandre Aubin <alex.aubin@mailoo.org>
2021-06-11 16:32:40 +0200

    Fix bullseye install : php-gettext -> php-php-gettext

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@ YNH_PHP_VERSION="7.3"
 
 pkg_dependencies="postgresql apt-transport-https libgd-dev"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-gd php-gettext"
+extra_php_dependencies="php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-gd php-php-gettext"
 
 #=================================================
 # COMMON HELPERS
